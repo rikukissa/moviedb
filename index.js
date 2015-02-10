ad9e7b0c0d98e97b266672af750ed2c84fc221a2
Riku Rouvila <riku.rouvila@gmail.com>
2015-02-10 12:39:07 +0200

    implement mongodb support

--- index.js
+++ index.js
@@ -1,36 +1,55 @@
 'use strict';
 
 var express = require('express');
+var MongoClient = require('mongodb').MongoClient;
+
 var app = express(); // Luodaan uusi express-applikaatio
 
 var bodyParser = require('body-parser');
+
 app.use(bodyParser.json());
 
-var movies = [{ // Väliaikainen tietokanta elokuville
-  title: 'Robocop',
-  year: 1987
-}, {
-  title: 'Predator',
-  year: 1987
-}];
-
 // Endpoint kaikkien elokuvien hakemiseen
-app.get('/movies', function(req, res) {
-  res.send(movies);
+app.get('/movies', function(req, res, next) {
+  app.movies.find().toArray(function(err, results) {
+    if(err) {
+      next(err);
+      return;
+    }
+    res.send(results);
+  });
 });
 
 // Endpoint elokuvien tallentamiseen
-app.post('/movies', function(req, res) {
+app.post('/movies', function(req, res, next) {
   var newMovie = req.body;
-  newMovie.id = movies.length + 1;
 
-  movies.push(newMovie);
+  app.movies.insert(newMovie, function(err, newMovies) {
 
-  res.status(201).send(newMovie);
+    if(err) {
+      next(err);
+      return;
+    }
+
+    res.status(201).send(newMovies);
+  });
+
 });
 
-// Asetetaan applikaatio kuuntelemana porttia 9000 ja
-// tulostetaan viesti kun se on valmis vastaanottamaan kyselyitä
-app.listen(9000, function() {
-  console.log('Movie API listening port 9000');
+app.use(function(err, req, res, next) {
+  res.status(500).send('Internal server error');
+});
+
+MongoClient.connect('mongodb://127.0.0.1:27017/moviedb', function(err, db) {
+  if(err) {
+    throw err;
+  }
+  app.movies = db.collection('movies');
+
+  // Asetetaan applikaatio kuuntelemana porttia 9000 ja
+  // tulostetaan viesti kun se on valmis vastaanottamaan kyselyitä
+
+  app.listen(9000, function() {
+    console.log('Movie API listening port 9000');
+  });
 });
